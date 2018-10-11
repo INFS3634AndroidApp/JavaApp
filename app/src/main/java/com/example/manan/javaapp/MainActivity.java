8a019102502f050164ca42b4083029bf02df0e43
Jono <jonathan.silei.zhang@gmail.com>
2018-10-12 09:55:01 +1100

    Jonathan's Part (Adding Content To Scrollview)
    Added all the .txt files to the assets folder
    Had a method read() to read the topic 1 file (and others later on) and then place in a textview.

--- app/src/main/java/com/example/manan/javaapp/MainActivity.java
+++ app/src/main/java/com/example/manan/javaapp/MainActivity.java
@@ -2,12 +2,57 @@ package com.example.manan.javaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private TextView textView;
+    private StringBuilder text = new StringBuilder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //invoke read method
+        read("Topic 1.txt");
+    }
+
+
+
+    //Method Used to Read .txt file from Assets Folder
+    public void read(String filename) {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(
+                    new InputStreamReader(getAssets().open(filename)));
+
+
+            String mLine;
+            while ((mLine = reader.readLine()) != null) {
+                text.append(mLine);
+                text.append('\n');
+            }
+        } catch (IOException e) {
+            Toast.makeText(getApplicationContext(), "Error reading file!", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+
+                }
+            }
+        }
+        // Setting the content of .txt file into the Textview (located inside scrollview)
+        textView = findViewById(R.id.reader);
+        textView.setText(text);
     }
 }
